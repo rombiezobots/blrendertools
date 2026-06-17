080bbdf674ef21eaf0bd3c5f6cd72d03bfe3a964
Sam Van Hulle <sam@rombiezobots.com>
2026-06-17 16:06:54 +0200

    dissolved main menu into context menus

--- menus.py
+++ menus.py
@@ -11,9 +11,18 @@ import bpy
 ########################################################################################################################
 
 
-def draw_main_menu(self, context):
+def draw_menu_view3d(self, context):
+    '''Draw the viewport's context menu'''
     lay = self.layout
-    lay.menu(TOPBAR_MT_blrendertools.bl_idname)
+    lay.separator()
+    lay.menu('VIEW3D_MT_blrendertools')
+
+
+def draw_menu_node_editor(self, context):
+    '''Draw the node editor's context menu'''
+    lay = self.layout
+    lay.separator()
+    lay.menu('NODE_MT_blrendertools')
 
 
 ########################################################################################################################
@@ -21,18 +30,23 @@ def draw_main_menu(self, context):
 ########################################################################################################################
 
 
-class TOPBAR_MT_blrendertools(bpy.types.Menu):
+class VIEW3D_MT_blrendertools(bpy.types.Menu):
     bl_label = 'blrendertools'
-    bl_idname = 'TOPBAR_MT_blrendertools'
+    bl_idname = 'VIEW3D_MT_blrendertools'
 
     def draw(self, context):
         lay = self.layout
-        lay.label(text='Models')
-        lay.operator('blrendertools.manage_subdivision', icon='MOD_SUBSURF')
-        lay.label(text='Materials')
-        lay.operator('blrendertools.create_material')
-        lay.label(text='Images')
-        lay.operator('blrendertools.manage_image_sequences')
+        lay.operator('blrendertools.manage_subdivision', icon='MOD_SUBSURF', text='Manage Subdivision...')
+        lay.operator('blrendertools.create_material', text='Create New Material...')
+
+
+class NODE_MT_blrendertools(bpy.types.Menu):
+    bl_label = 'blrendertools'
+    bl_idname = 'NODE_MT_blrendertools'
+
+    def draw(self, context):
+        lay = self.layout
+        lay.operator('blrendertools.manage_image_sequences', text='Manage Image Sequences...')
 
 
 ########################################################################################################################
@@ -40,11 +54,21 @@ class TOPBAR_MT_blrendertools(bpy.types.Menu):
 ########################################################################################################################
 
 
+classes = [
+    VIEW3D_MT_blrendertools,
+    NODE_MT_blrendertools,
+]
+
+
 def register():
-    bpy.utils.register_class(TOPBAR_MT_blrendertools)
-    bpy.types.TOPBAR_MT_editor_menus.append(draw_main_menu)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.VIEW3D_MT_object_context_menu.append(draw_menu_view3d)
+    bpy.types.NODE_MT_context_menu.append(draw_menu_node_editor)
 
 
 def unregister():
-    bpy.types.TOPBAR_MT_editor_menus.remove(draw_main_menu)
-    bpy.utils.unregister_class(TOPBAR_MT_blrendertools)
+    bpy.types.NODE_MT_context_menu.remove(draw_menu_node_editor)
+    bpy.types.VIEW3D_MT_object_context_menu.remove(draw_menu_view3d)
+    for cls in reversed(classes):
+        bpy.utils.unregister_class(cls)
